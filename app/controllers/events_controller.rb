4b21015ff72beced54bd21709cb5ec9cb9be688c
Sambath Prum <psb.sambath@gmail.com>
2015-06-06 22:45:39 -0500

    add pagination

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,5 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all_active
+    @events = Event.all_active.order('due_at DESC').page(params[:page]).per(15)
   end
-
 end
