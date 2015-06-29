68efded8ba296b4ec62c680af9f695fa123d7c8f
Sambath Prum <psb.sambath@gmail.com>
2015-06-28 23:23:06 -0500

    loop through event_registrations association instead

--- app/models/event_registration.rb
+++ app/models/event_registration.rb
@@ -1,8 +1,4 @@
 class EventRegistration < ActiveRecord::Base
   belongs_to :participant, class_name: User, foreign_key: :user_id
   belongs_to :event
-
-  def self.get_id(event, participant)
-    where("event_id = ? and user_id = ?", event.id, participant.id).first.id
-  end
 end
